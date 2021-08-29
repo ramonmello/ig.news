bcae541f4dd167abbc722413a699e87e24143764
Ramon Mello <ramonomello@gmail.com>
2021-08-29 01:29:10 -0300

    feat: add webhook to listen payment events

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,53 +1,41 @@
-import { query as q } from 'faunadb'
+import { query as q } from "faunadb";
 
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
 
-import { fauna } from '../../../services/fauna'
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
-  providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-			scope: 'read:user'
-    }),
-  ],
+	providers: [
+		Providers.GitHub({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+			scope: "read:user",
+		}),
+	],
 
 	callbacks: {
 		async signIn(user, account, profile) {
-			const { email } = user
+			const { email } = user;
 
 			try {
 				await fauna.query(
 					q.If(
 						q.Not(
 							q.Exists(
-								q.Match(
-									q.Index('user_by_email'),
-									q.Casefold(user.email)
-								)
+								q.Match(q.Index("user_by_email"), q.Casefold(user.email))
 							)
 						),
-						q.Create(
-							q.Collection('users'),
-							{ data: { email } }
-						),
-						q.Get(
-							q.Match(
-								q.Index('user_by_email'),
-								q.Casefold(user.email)
-							)
-						)
+						q.Create(q.Collection("users"), { data: { email } }),
+						q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
 					)
+				);
 
-				)
-
-				return true
+				return true;
 			} catch (error) {
-				console.log(error)
-				return false
+				console.log(error);
+				return false;
 			}
-    },
-	}
-})
+		},
+	},
+});
