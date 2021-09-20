eee68a1633bf80a4c01d0cb798a90b53132954ce
Ramon Mello <ramonomello@gmail.com>
2021-09-20 01:47:03 -0300

    feat: create activeLink component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,25 @@
-import { SignInButton } from '../SignInButton'
-import styles from './styles.module.scss'
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 
-export function Header () {
-  return (
-    <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
-        <nav>
-					<a className={styles.active} href="//#endregion">Home</a>
-					<a href="//#region">Posts</a>
-        </nav>
+export function Header() {
+	return (
+		<header className={styles.headerContainer}>
+			<div className={styles.headerContent}>
+				<img src="/images/logo.svg" alt="ig.news" />
+				<nav>
+					<ActiveLink activeClassName={styles.active} href="/">
+						<a>Home</a>
+					</ActiveLink>
+					<ActiveLink activeClassName={styles.active} href="/posts">
+						<a>Posts</a>
+					</ActiveLink>
+				</nav>
 
 				<SignInButton />
-      </div>
-    </header>
-    );
+			</div>
+		</header>
+	);
 }
